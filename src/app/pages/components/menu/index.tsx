fff7c9e84c88f8b209988d9bf069f50f384acb72
JuanAmaral <trabalho@juanamaral.com.br>
2023-05-24 00:19:33 -0300

    fix/menu buttons
    userouter to patchame

--- src/app/pages/components/menu/index.tsx
+++ src/app/pages/components/menu/index.tsx
@@ -1,24 +1,35 @@
 "use client";
 import { useEffect, useState } from "react";
 import * as S from "../../style.ts/components/menu/style";
+import { usePathname } from "next/navigation";
 
 type MenuType = {
   selected: "contact" | "home" | "portfolio";
 };
+type ScrollAmounts = {
+  [key: string]: number[];
+};
+
 export default function FrameWinner({ selected }: MenuType) {
-  const documentContainer = document.getElementById("container");
+  const pathname = usePathname();
+  const [currentPath, setCurrentPath] = useState("");
 
+  useEffect(() => {
+    setCurrentPath(window.location.pathname);
+  }, []);
   function goPage(page: number) {
-    if (!documentContainer) return;
-    const scrollAmounts = {
-      contact: [0, 1502, 3004],
-      home: [-1502, 0, 1502],
-      portfolio: [-3004, -1502, 0],
-    };
-    const scrollByAmount = scrollAmounts[selected][page - 1];
-    documentContainer.scrollBy({ left: scrollByAmount });
+    if (typeof window !== "undefined") {
+      const documentContainer = document.getElementById("container");
+      if (!documentContainer) return;
+      const scrollAmounts = {
+        contact: [0, 1502, 3004],
+        home: [-1502, 0, 1502],
+        portfolio: [-3004, -1502, 0],
+      };
+      const scrollByAmount = scrollAmounts[selected][page - 1];
+      documentContainer.scrollBy({ left: scrollByAmount });
+    }
   }
-
   return (
     <S.Menu>
       <S.MenuItem
